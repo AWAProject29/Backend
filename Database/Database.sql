5fcfd07e365e4d67eba56c34f3fef4a23162bcbd
Danskutin <dante.leino@gmail.com>
2021-11-10 14:50:44 +0200

    Database updated

--- Database/Database.sql
+++ Database/Database.sql
@@ -33,7 +33,7 @@ CREATE TABLE `customer` (
   UNIQUE KEY `email_UNIQUE` (`email`),
   KEY `idorder` (`idorder`) /*!80000 INVISIBLE */,
   CONSTRAINT `fk_idorder2` FOREIGN KEY (`idorder`) REFERENCES `order` (`idorder`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,6 @@ CREATE TABLE `customer` (
 
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` VALUES (1,NULL,'Tuomas','Riihiaho','tuomas@miukumauku','salasana');
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,13 +54,15 @@ DROP TABLE IF EXISTS `manager`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `manager` (
   `idmanager` int(11) NOT NULL AUTO_INCREMENT,
-  `idrestaurant` int(11) NOT NULL,
-  `idorder` int(11) NOT NULL,
+  `idrestaurant` int(11) DEFAULT NULL,
+  `idorder` int(11) DEFAULT NULL,
   `firstname` varchar(45) NOT NULL,
   `lastname` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
+  `managerauthentication` varchar(45) NOT NULL,
   PRIMARY KEY (`idmanager`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
   KEY `idrestaurant` (`idrestaurant`) /*!80000 INVISIBLE */,
   KEY `idorder` (`idorder`) /*!80000 INVISIBLE */,
   CONSTRAINT `fk_idorder` FOREIGN KEY (`idorder`) REFERENCES `order` (`idorder`) ON DELETE RESTRICT ON UPDATE CASCADE,
@@ -87,10 +88,7 @@ DROP TABLE IF EXISTS `menu`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `menu` (
   `idmenu` int(11) NOT NULL AUTO_INCREMENT,
-  `productname` varchar(45) NOT NULL,
-  `productprice` float NOT NULL,
-  `productdescription` varchar(45) DEFAULT NULL,
-  `productimage` blob,
+  `menudescription` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idmenu`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -105,6 +103,33 @@ LOCK TABLES `menu` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `menu_has_product`
+--
+
+DROP TABLE IF EXISTS `menu_has_product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `menu_has_product` (
+  `menu_idmenu` int(11) NOT NULL,
+  `product_productid` int(11) NOT NULL,
+  PRIMARY KEY (`menu_idmenu`,`product_productid`),
+  KEY `fk_menu_has_product_product1_idx` (`product_productid`),
+  KEY `fk_menu_has_product_menu1_idx` (`menu_idmenu`),
+  CONSTRAINT `fk_menu_has_product_menu1` FOREIGN KEY (`menu_idmenu`) REFERENCES `menu` (`idmenu`),
+  CONSTRAINT `fk_menu_has_product_product1` FOREIGN KEY (`product_productid`) REFERENCES `product` (`productid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu_has_product`
+--
+
+LOCK TABLES `menu_has_product` WRITE;
+/*!40000 ALTER TABLE `menu_has_product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu_has_product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `order`
 --
 
@@ -113,15 +138,14 @@ DROP TABLE IF EXISTS `order`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `order` (
   `idorder` int(11) NOT NULL AUTO_INCREMENT,
-  `idmenu` int(11) NOT NULL,
-  `eta` datetime DEFAULT NULL COMMENT 'Format of DATETIME data type is (yyyy-mm-dd hh:mm:ss)\n\nProbably have to alter data within the code to only include (hh:mm:ss) in the ETA of the order.',
-  `status` varchar(45) DEFAULT NULL COMMENT 'Order received\nPreparing order\nReady for delivery\nDelivering\nDelivered',
+  `idshoppingcart` int(11) NOT NULL,
+  `eta` datetime DEFAULT NULL COMMENT 'Format of DATETIME data type is (yyyy-mm-dd hh:mm:ss)\\n\\nProbably have to alter data within the code to only include (hh:mm:ss) in the ETA of the order.',
+  `status` varchar(45) DEFAULT NULL COMMENT 'Order received\\nPreparing order\\nReady for delivery\\nDelivering\\nDelivered',
   `deliverylocation` varchar(45) DEFAULT NULL,
   `cost` float DEFAULT NULL,
-  `Ordercol` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idorder`),
-  KEY `idmenu` (`idmenu`) /*!80000 INVISIBLE */,
-  CONSTRAINT `fk_idmenu2` FOREIGN KEY (`idmenu`) REFERENCES `menu` (`idmenu`) ON DELETE RESTRICT ON UPDATE CASCADE
+  KEY `fk_order_shoppingcart1_idx` (`idshoppingcart`),
+  CONSTRAINT `fk_order_shoppingcart1` FOREIGN KEY (`idshoppingcart`) REFERENCES `shoppingcart` (`idshoppingcart`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -135,6 +159,33 @@ LOCK TABLES `order` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `productid` int(11) NOT NULL,
+  `productname` varchar(45) NOT NULL,
+  `productprice` float NOT NULL,
+  `productdescription` varchar(45) DEFAULT NULL,
+  `productimage` blob,
+  PRIMARY KEY (`productid`),
+  UNIQUE KEY `productname_UNIQUE` (`productname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `restaurant`
 --
 
@@ -143,15 +194,16 @@ DROP TABLE IF EXISTS `restaurant`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `restaurant` (
   `idrestaurant` int(11) NOT NULL AUTO_INCREMENT,
-  `idmenu` int(11) NOT NULL,
-  `name` varchar(45) NOT NULL,
+  `idmenu` int(11) DEFAULT NULL,
+  `restaurantname` varchar(45) NOT NULL,
   `address` varchar(45) NOT NULL,
-  `type` varchar(45) DEFAULT NULL,
+  `restauranttype` varchar(45) DEFAULT NULL,
   `pricelevel` varchar(45) DEFAULT NULL,
   `operatinghours` varchar(45) DEFAULT NULL COMMENT '10.30-22.30',
   `restaurantimage` blob,
-  `restaurantdescription` varchar(100) DEFAULT NULL,
+  `restaurantdescription` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`idrestaurant`),
+  UNIQUE KEY `restaurantname_UNIQUE` (`restaurantname`),
   KEY `idmenu` (`idmenu`) /*!80000 INVISIBLE */,
   CONSTRAINT `fk_idmenu` FOREIGN KEY (`idmenu`) REFERENCES `menu` (`idmenu`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -165,6 +217,216 @@ LOCK TABLES `restaurant` WRITE;
 /*!40000 ALTER TABLE `restaurant` DISABLE KEYS */;
 /*!40000 ALTER TABLE `restaurant` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `shoppingcart`
+--
+
+DROP TABLE IF EXISTS `shoppingcart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shoppingcart` (
+  `idshoppingcart` int(11) NOT NULL,
+  `shoppingcartcol` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idshoppingcart`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shoppingcart`
+--
+
+LOCK TABLES `shoppingcart` WRITE;
+/*!40000 ALTER TABLE `shoppingcart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shoppingcart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shoppingcart_has_product`
+--
+
+DROP TABLE IF EXISTS `shoppingcart_has_product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shoppingcart_has_product` (
+  `shoppingcart_idshoppingcart` int(11) NOT NULL,
+  `product_productid` int(11) NOT NULL,
+  PRIMARY KEY (`shoppingcart_idshoppingcart`,`product_productid`),
+  KEY `fk_shoppingcart_has_product_product1_idx` (`product_productid`),
+  KEY `fk_shoppingcart_has_product_shoppingcart1_idx` (`shoppingcart_idshoppingcart`),
+  CONSTRAINT `fk_shoppingcart_has_product_product1` FOREIGN KEY (`product_productid`) REFERENCES `product` (`productid`),
+  CONSTRAINT `fk_shoppingcart_has_product_shoppingcart1` FOREIGN KEY (`shoppingcart_idshoppingcart`) REFERENCES `shoppingcart` (`idshoppingcart`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shoppingcart_has_product`
+--
+
+LOCK TABLES `shoppingcart_has_product` WRITE;
+/*!40000 ALTER TABLE `shoppingcart_has_product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shoppingcart_has_product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'hermes_database'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `addCustomer` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`Daniel1`@`localhost` PROCEDURE `addCustomer`(
+	IN idcustomer INT,
+    IN idorder INT,
+	IN firstname VARCHAR(45),
+	IN lastname VARCHAR(45),
+	IN email VARCHAR(45),
+	IN pw VARCHAR(45)
+)
+BEGIN
+	INSERT INTO Customer VALUES (idcustomer, idorder, firstname, lastname, email, pw);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `addManager` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`Daniel1`@`localhost` PROCEDURE `addManager`(
+	IN idmanager INT,
+    IN idrestaurant INT,
+    IN idorder INT,
+	IN firstname VARCHAR(45),
+	IN lastname VARCHAR(45),
+	IN email VARCHAR(45),
+	IN pw VARCHAR(45),
+    IN managerauthentication VARCHAR(45)
+)
+INSERT INTO manager VALUES (idmanager, idrestaurant, idorder, firstname, lastname, email, pw, managerauthentication); ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `addRestaurant` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`Daniel1`@`localhost` PROCEDURE `addRestaurant`(
+	IN idrestaurant INT,
+    IN idmenu INT,
+    IN restaurantname VARCHAR(45),
+	IN address VARCHAR(45),
+	IN restauranttype VARCHAR(45),
+	IN pricelevel VARCHAR(45),
+	IN operatinghours VARCHAR(45),
+    IN restaurantimage BLOB,
+    IN restaurantdescription VARCHAR(200)
+)
+INSERT INTO restaurant VALUES (idrestaurant, idmenu, restaurantname, address, restauranttype, 
+    pricelevel, operatinghours, restaurantimage, restaurantdescription); ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `searchRestaurant` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`Daniel1`@`localhost` PROCEDURE `searchRestaurant`(
+	IN searchstring VARCHAR(45)
+)
+SELECT * FROM restaurant WHERE restaurantname LIKE CONCAT('%', searchstring, '%') OR address LIKE CONCAT('%', searchstring, '%') OR restauranttype LIKE CONCAT('%', searchstring, '%'); ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `showCustomers` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`Daniel1`@`localhost` PROCEDURE `showCustomers`()
+BEGIN
+    SELECT *  FROM Customer;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `showManagers` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`Daniel1`@`localhost` PROCEDURE `showManagers`()
+BEGIN
+    SELECT *  FROM manager;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `showRestaurants` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`Daniel1`@`localhost` PROCEDURE `showRestaurants`()
+BEGIN
+    SELECT *  FROM restaurant;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -175,4 +437,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-09 11:53:40
+-- Dump completed on 2021-11-10 14:45:46

4e7dfdcc404aa2ae123b57eeaa3bcbb002f4022a
Tuomas Riihiaho <78533799+c0ritu00@users.noreply.github.com>
2021-11-24 10:15:22 +0200

    Added JWT base structure to index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const passport = require('passport');
 const BasicStrategy = require('passport-http').BasicStrategy; 
 const { v4: uuidv4 } = require('uuid');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JwtStrategy = require('passport-jwt').Strategy,
+      ExtractJwt = require('passport-jwt').ExtractJwt;
 const db = require('../database');
 
 app.use(express.json());
@@ -32,7 +35,6 @@ const users = [
     },
 ]; 
 
-<<<<<<< HEAD
 passport.use(new BasicStrategy(
     function(email, password, done) {
 
@@ -47,7 +49,7 @@ passport.use(new BasicStrategy(
       }
 
       /* Verify password match */
-      if(bcrypt.compareSync(password, customer.password) == false && bcrypt.compareSync(email, customer.email) == false) {
+      if(bcrypt.compareSync(password, customer.password) == false && (email == customer.email) == false) {
         // Password does not match
         console.log("HTTP Basic password not matching username");
         return done(null, false, { message: "HTTP Basic password not found" });
@@ -55,37 +57,21 @@ passport.use(new BasicStrategy(
       return done(null, customer);
     }
   ));
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: "MyVerySecretSigningKey"
+}
+
+passport.use(new JwtStrategy(jwtOptions, function(jwt_payload, done){
+  console.log('JWT is valid');
+  console.log('payload is as follows');
+  console.log(jwt_payload);
+
+  done(null, jwt_payload);
+}));
+
      
-=======
-
-    passport.use(new BasicStrategy(
-        function(email, password, done) {
-      
-          const userExists = db.query(
-            'CALL checkAccount(?,?)',
-            [customer.email, customer.password, id], callback
-          );
-          if(userExists == 'FALSE') {
-            // Username not found
-            console.log("HTTP Basic username not found");
-            return done(null, false, { message: "HTTP Basic user not found" });
-          }
-      
-          /* Verify password match */
-          if(bcrypt.compareSync(password, customer.password) == false) {
-            // Password does not match
-            console.log("HTTP Basic password not matching username");
-            return done(null, false, { message: "HTTP Basic password not found" });
-          }
-          return done(null, userExists);
-        }
-      ));
-
-
-
-
-
->>>>>>> 559c6ce8eea400cfeb2f2f4c9c8bdf4fb6976d0c
 function httpBasicTest(req, res, next) {
     console.log('http basic test executing...');
     next();
@@ -126,7 +112,36 @@ app.get('/some-other-resource', passport.authenticate('basic', { session: false 
     res.send('Other protected resource accessed');
 })
 
+app.post('/jwtLogin', passport.authenticate('basic', { session: false }), (req, res) => {
+  //generate JWT
 
+  //Change for information from SQL table
+  const payload = { 
+    user: {
+      id: req.user.id,
+      username: req.user.username
+    }
+  };
+
+  const secretKey = "MyVerySecretSigningKey";
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  const generatedJWT = jwt.sign(payload, secretKey, options)
+
+
+  res.json({ jwt: generatedJWT });
+})
+
+
+app.get('/jwt-protected-resource',  passport.authenticate('jwt', { session: false }), (req, res) => {
+  //change for correct sql term
+  console.log(req.user);
+
+  res.send("OK, for user " + req.user.user.username);
+})
 
 
 app.listen(port, () => {

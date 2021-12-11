5d94a5bd7926eea434d9d74b4c72043572e2f456
Tuomas Riihiaho <78533799+c0ritu00@users.noreply.github.com>
2021-12-11 16:32:25 +0200

    test

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ var app = express();
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json());
+app.use(function (req, res, next){
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
+   next();
+});
 
 app.use(cors({
     // origin: "https://hermes-project-group-29.herokuapp.com/*",
